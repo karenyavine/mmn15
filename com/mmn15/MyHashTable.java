bd628009e3baf45b38774dab01ba9c7c88f320d5
Ohad Shulz <ohad@shultzu.com>
2019-06-30 16:56:50 +0300

    test with sourcetree

--- com/mmn15/MyHashTable.java
+++ com/mmn15/MyHashTable.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 
 public class MyHashTable {
 
-    private int tableSize = 710000001;
+    private int tableSize = 710000000;
 
     Item[] values = new Item[tableSize];
 
